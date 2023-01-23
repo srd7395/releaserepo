c8ce45958afc13540d825a867cfe67461fd321e7
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-01-23 02:46:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "rgr" {
-   name = "Pipelinergrelease"
+resource "azurerm_resource_group" "rgr1" {
+   name = "Pipelinergrelease2"
    location = "Canada Central"
   
 }
 
-resource "azurerm_public_ip" "rgr" {
-    name = "releasepip"
-    resource_group_name = azurerm_resource_group.rgr.name
-    location = azurerm_resource_group.rgr.location
+resource "azurerm_public_ip" "rgr1" {
+    name = "releasepip2"
+    resource_group_name = azurerm_resource_group.rgr1.name
+    location = azurerm_resource_group.rgr1.location
     allocation_method   = "Static"
 
   tags = {
